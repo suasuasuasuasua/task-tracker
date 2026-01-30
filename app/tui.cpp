6a82975232b60f364018ab05a2a981011f213828
Justin Hoang <justinhoang@sua.dev>
2026-01-30 12:18:11 -0700

    chore: slightly more interesting tui

--- app/tui.cpp
+++ app/tui.cpp
@@ -1,43 +1,61 @@
-#include <ftxui/dom/node.hpp>
-#include <string> // for char_traits, operator+, string, basic_string
-#include <vector>
-
 #include "ftxui/component/component.hpp"      // for Input, Renderer, Vertical
 #include "ftxui/component/component_base.hpp" // for ComponentBase
-#include "ftxui/component/component_options.hpp" // for InputOption
 #include "ftxui/component/screen_interactive.hpp" // for Component, ScreenInteractive
 #include "ftxui/dom/elements.hpp" // for text, hbox, separator, Element, operator|, vbox, border
-#include "ftxui/util/ref.hpp" // for Ref
+#include <ftxui/dom/deprecated.hpp>
+#include <ftxui/dom/node.hpp>
+#include <ranges>
+#include <vector>
+
+using namespace ftxui;
+
+ComponentDecorator handler_quit(ScreenInteractive &screen) {
+  return CatchEvent([&](Event event) {
+    if (event == Event::q or event == Event::Q) {
+      screen.ExitLoopClosure()();
+      return true;
+    }
+    return false;
+  });
+}
 
 int main(int argc, char *argv[]) {
-  using namespace ftxui;
-
-  // The data:
-  std::string task;
   std::vector<std::string> tasks;
+  std::string cur_task;
+  std::vector<Element> ts;
 
-  // The basic input components:
-  Component input_task = Input(&task, "write a short description");
-
-  input_task |= CatchEvent(
-      [&](Event event) { return event.is_character() && task.size() > 80; });
-
-  // The component tree:
-  auto component = Container::Vertical({input_task});
-
-  // Tweak how the component tree is rendered:
-  auto renderer = Renderer(component, [&] {
-    return vbox({
-               window(text("tasks"),
-                      vbox({
-                          hbox(text("input: "), input_task->Render()),
-                          separator(),
-                          text("task: " + task),
-                      })),
-           }) |
-           border;
+  Component input_task = Input(&cur_task, "Task:");
+  input_task |= CatchEvent([&tasks, &cur_task](Event event) {
+    if (event == Event::Return) {
+      tasks.push_back(cur_task);
+      cur_task.clear();
+      return true;
+    }
+    return false;
   });
 
   auto screen = ScreenInteractive::Fullscreen();
+  auto component = Container::Vertical({input_task});
+  auto renderer = Renderer(component, [&] {
+    ts = std::vector<Element>(tasks.size());
+    std::transform(tasks.begin(), tasks.end(), ts.begin(),
+                   [](const auto &t) { return text(t); });
+
+    auto display_ele = vbox({
+        text("Enter a new task"),
+        input_task->Render(),
+        vbox(ts),
+        hbox({
+            text("left") | border,
+            text("middle") | border,
+            text("right") | border,
+        }),
+        separator(),
+        text("Press (q) to quit"),
+    });
+
+    return vbox({window(text("Task Tracker"), display_ele)});
+  });
+  // renderer | handler_quit(screen);
   screen.Loop(renderer);
 }
---
+++ docs/ftxui.md
@@ -0,0 +1,6 @@
+# FTXUI notes
+
+## Links
+
+- [Live docs](https://arthursonzogni.github.io/FTXUI/)
+- [Github link](https://github.com/ArthurSonzogni/FTXUI)
